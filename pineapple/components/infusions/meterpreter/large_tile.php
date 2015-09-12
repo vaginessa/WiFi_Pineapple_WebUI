376dc8e0cff18d24a11cea7dc58d6073efc51dc2
Jack Davies <lavanoid@gmail.com>
2015-09-12 03:43:19 +0100

    Those shouldn't be there.

--- pineapple/components/infusions/meterpreter/large_tile.php
+++
@@ -1,9 +0,0 @@
-<script type='text/javascript' src='/components/infusions/meterpreter/includes/helpers.js'></script>
-<style>@import url('<?=$rel_dir?>includes/styles.css')</style>
-
-<ul id="tabs">
-  <li><a id="config">Configuration</a></li>
-  <li><a id="help">Help</a></li>
-</ul>
-<div class="tabContainer" ></div>
-
